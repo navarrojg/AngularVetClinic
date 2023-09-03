bc903843bc4c76194c41eb3d0eb8750e42b89098
bartek <taras_basket@wp.pl>
2023-09-03 19:32:23 +0200

    new components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PatientsComponent } from './patients/patients.component';
+import { HeaderComponent } from './header/header.component';
+import { ImportantNotesComponent } from './important-notes/important-notes.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, PatientsComponent, HeaderComponent, ImportantNotesComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/important-notes/important-notes.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-important-notes',
+  templateUrl: './important-notes.component.html',
+  styleUrls: ['./important-notes.component.css']
+})
+export class ImportantNotesComponent {
+
+}
