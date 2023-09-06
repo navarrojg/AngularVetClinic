c5ba5c36b21f0bef6253ee920747cd4255809164
bartek <taras_basket@wp.pl>
2023-09-06 22:56:34 +0200

    new patients added

--- src/app/patients/patient-list/patient-list.component.ts
+++ src/app/patients/patient-list/patient-list.component.ts
@@ -17,6 +17,25 @@ export class PatientListComponent {
       'A+',
       [new Medicine('Apap', 2, 'daily')]
     ),
+    new Patient(
+      'CrackerJack',
+      4,
+      'hips problem',
+      'https://www.zooplus.pl/magazyn/wp-content/uploads/2022/05/kon-oldenburski-sylwetka-1024x683.jpeg',
+      'A-',
+      [new Medicine('Vicodin', 1, 'two times per day')]
+    ),
+    new Patient(
+      'Hope',
+      9,
+      'bitten by snake',
+      'https://www.fizjoterapiakoni.com.pl/wp-content/uploads/2019/01/Czy-Tw%C3%B3j-ko%C5%84-potrzebuje-fizjo.jpg',
+      'A-',
+      [
+        new Medicine('Akatar', 0.5, 'in the evening'),
+        new Medicine('Colaflex', 2, 'two times per day'),
+      ]
+    ),
   ];
 
   onNewRecipe() {}
