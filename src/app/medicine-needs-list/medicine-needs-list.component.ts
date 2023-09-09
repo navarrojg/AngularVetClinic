ef36c6ffd2330b2b9420d7d8e73012cde19c2e4f
bartek <taras_basket@wp.pl>
2023-09-09 15:45:02 +0200

    working on medicineNeeds component, empty medicine service linked

--- src/app/medicine-needs-list/medicine-needs-list.component.ts
+++ src/app/medicine-needs-list/medicine-needs-list.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-medicine-needs-list',
   templateUrl: './medicine-needs-list.component.html',
-  styleUrls: ['./medicine-needs-list.component.css']
+  styleUrls: ['./medicine-needs-list.component.css'],
 })
 export class MedicineNeedsListComponent {
-
+  medications = [];
 }
