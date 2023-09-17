ebf245e0175845c4ffeb2e17ebe435935494542d
bartek <taras_basket@wp.pl>
2023-09-17 15:31:50 +0200

    working on medicineListForm

--- src/app/medicine-needs-list/medicine-needs-list-edit/medicine-needs-list-edit.component.ts
+++ src/app/medicine-needs-list/medicine-needs-list-edit/medicine-needs-list-edit.component.ts
@@ -2,6 +2,7 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
 import { Subject } from 'rxjs';
 import { Medicine } from 'src/app/shared/medicine.model';
 import { MedicineNeedsListService } from '../medicine-needs-list.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-medicine-needs-list-edit',
@@ -9,22 +10,16 @@ import { MedicineNeedsListService } from '../medicine-needs-list.service';
   styleUrls: ['./medicine-needs-list-edit.component.css'],
 })
 export class MedicineNeedsListEditComponent {
-  @ViewChild('nameInput', { static: false }) nameInput: ElementRef;
-  @ViewChild('amountInput', { static: false }) amountInput: ElementRef;
-  @ViewChild('freqInput', { static: false }) freqInput: ElementRef;
+  @ViewChild('f', { static: false }) slForm: NgForm;
   medicineAdded = new Subject<Medicine>();
 
   constructor(private medicineService: MedicineNeedsListService) {}
 
-  onAddMed() {
-    const medName = this.nameInput.nativeElement.value;
-    const medAmount = this.amountInput.nativeElement.value;
-    const medFreq = this.freqInput.nativeElement.value;
-    const newMed = new Medicine(medName, medAmount, medFreq);
-    this.medicineService.addNewMedicine(newMed);
-  }
+  onAddMed() {}
 
   onEditMed() {}
 
   onClear() {}
+
+  onSubmit(form: NgForm) {}
 }
