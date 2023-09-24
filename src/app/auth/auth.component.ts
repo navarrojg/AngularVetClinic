cff4eaa804e671b914cf47c914e5de2fdb615204
bartek <taras_basket@wp.pl>
2023-09-24 18:28:49 +0200

    creating AuthService and setting signup methond

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,18 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { AuthService } from './auth.service';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
 })
 export class AuthComponent {
+  // @ViewChild('autForm', { static: false }) authFrom: NgForm;
   isloginMode = true;
+
+  constructor(private authService: AuthService) {}
 
   onSwitchMode() {
     this.isloginMode = !this.isloginMode;
   }
   onSubmit(form: NgForm) {
-    console.log(form.value);
+    if (!form.valid) {
+      return;
+    }
+    const email = form.value.email;
+    const password = form.value.password;
+
+    if (this.isloginMode) {
+      //..
+    } else {
+      this.authService.signup(email, password).subscribe(
+        (resData) => {
+          console.log(resData);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+    }
+
     form.reset();
   }
 }
