89df5a6f5186f7b3bf494177cc68f832ee25191b
bartek <taras_basket@wp.pl>
2023-09-29 19:12:43 +0200

    working on archiveItemComponent

--- src/app/archive/archive-item/archive-item.component.ts
+++ src/app/archive/archive-item/archive-item.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Patient } from 'src/app/patients/patient.model';
 
 @Component({
   selector: 'app-archive-item',
   templateUrl: './archive-item.component.html',
-  styleUrls: ['./archive-item.component.css']
+  styleUrls: ['./archive-item.component.css'],
 })
 export class ArchiveItemComponent {
-
+  @Input() archivedPatient: Patient;
+  @Input() id: number;
 }
