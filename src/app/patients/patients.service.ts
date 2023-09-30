6e05a9c472ed6060155214249c4d46d4bbbcf428
bartek <taras_basket@wp.pl>
2023-09-30 14:12:16 +0200

    styling archiveItems

--- src/app/patients/patients.service.ts
+++ src/app/patients/patients.service.ts
@@ -43,9 +43,42 @@ export class PatientsService {
   //   ),
   // ];
 
+  private archivedPatients: Patient[] =[
+        new Patient(
+      'Bojack',
+      6,
+      'M',
+      'broken left back leg',
+      'https://horseyhooves.com/wp-content/uploads/2022/08/Beautiful-horse-running-in-a-grassy-field.jpg',
+      'A+',
+      [new Medicine('Apap', 2, 'daily')]
+    ),
+    new Patient(
+      'CrackerJack',
+      4,
+      'M',
+      'hips problem',
+      'https://www.zooplus.pl/magazyn/wp-content/uploads/2022/05/kon-oldenburski-sylwetka-1024x683.jpeg',
+      'A-',
+      [new Medicine('Vicodin', 1, 'two times per day')]
+    ),
+    new Patient(
+      'Hope',
+      9,
+      'F',
+      'bitten by snake',
+      'https://www.fizjoterapiakoni.com.pl/wp-content/uploads/2019/01/Czy-Tw%C3%B3j-ko%C5%84-potrzebuje-fizjo.jpg',
+      'A-',
+      [
+        new Medicine('Akatar', 0.5, 'in the evening'),
+        new Medicine('Colaflex', 2, 'two times per day'),
+      ]
+    ),
+  ]
+
   private patients: Patient[] = [];
 
-  private archivedPatients: Patient[] = [];
+  // private archivedPatients: Patient[] = [];
 
   constructor(
     private medListService: MedicineNeedsListService,
